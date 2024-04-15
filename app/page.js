4f6edcada8c4c96cd9c30cc10e9c1ab0b9614dc9
devhumayun <humayunkabir9408@gmail.com>
2024-04-15 21:48:10 +0600

    geo location detector function create

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import LocationDetector from "@/components/LocationDetector";
+
 export default function Home() {
-  return <h1 className="text-red-800">Home</h1>;
+  return <LocationDetector />;
 }
